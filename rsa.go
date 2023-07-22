3b124b34d53495c9ec9adb0e8f2e2d0bb860610a
Kevin Falting <kevinfalting@gmail.com>
2023-07-22 15:03:28 -0500

    add RSAHandler for handling secrets

---
+++ rsa.go
@@ -0,0 +1,175 @@
+package structconf
+
+import (
+	"context"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha256"
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/pem"
+	"errors"
+	"fmt"
+)
+
+// RSA is a struct that holds a pair of RSA keys and provides methods for
+// handling RSA cryptographic operations. This handler is best used as the final
+// handler.
+type RSAHandler struct {
+	PrivateKey *rsa.PrivateKey
+	PublicKey  *rsa.PublicKey
+
+	// Label is an optional byte slice used during encryption and decryption
+	// with RSA, and enhances message integrity checks. The same Label must be
+	// used for both encryption and decryption if one is set.
+	// Note: The Label is not secret and does not get encrypted.
+	Label []byte
+}
+
+var _ Handler = (*RSAHandler)(nil)
+
+// Handle applies the RSA decryption process to a given field value. It checks
+// for the "secret" tag and decrypts the value if found. If a previous handler
+// has attempted to set a value, represented by the interimValue, this handler
+// will decrypt the interimValue, otherwise it will decrypt the field value.
+func (r *RSAHandler) Handle(ctx context.Context, f Field, interimValue any) (any, error) {
+	_, ok := f.LookupTag("conf", "secret")
+	if !ok {
+		return nil, nil
+	}
+
+	val := f.Value()
+	if interimValue != nil {
+		val = interimValue
+	}
+
+	ciphertext, ok := val.(string)
+	if !ok {
+		return nil, fmt.Errorf("field %q or interimValue must be of type string, got: %T", f.Name(), val)
+	}
+
+	if len(ciphertext) == 0 {
+		// If there is nothing to decrypt, that's okay too.
+		return nil, nil
+	}
+
+	plaintext, err := r.Decrypt([]byte(ciphertext))
+	if err != nil {
+		return nil, err
+	}
+
+	return string(plaintext), nil
+}
+
+// Decrypt decodes the provided ciphertext from base64 and decrypts it using RSA OAEP.
+func (r *RSAHandler) Decrypt(cipherText []byte) ([]byte, error) {
+	encryptedBytes := make([]byte, base64.StdEncoding.DecodedLen(len(cipherText)))
+	n, err := base64.StdEncoding.Decode(encryptedBytes, cipherText)
+	if err != nil {
+		return nil, err
+	}
+
+	encryptedBytes = encryptedBytes[:n]
+
+	decryptedBytes, err := rsa.DecryptOAEP(
+		sha256.New(),
+		nil,
+		r.PrivateKey,
+		encryptedBytes,
+		r.Label,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return decryptedBytes, nil
+}
+
+// Encrypt encrypts the plaintext using RSA OAEP, then encodes the ciphertext into base64.
+func (r *RSAHandler) Encrypt(plaintext []byte) ([]byte, error) {
+	encryptedBytes, err := rsa.EncryptOAEP(
+		sha256.New(),
+		rand.Reader,
+		r.PublicKey,
+		plaintext,
+		r.Label,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	cipherText := make([]byte, base64.StdEncoding.EncodedLen(len(encryptedBytes)))
+	base64.StdEncoding.Encode(cipherText, encryptedBytes)
+
+	return cipherText, nil
+}
+
+// NewPEMKeyPair generates a new pair of RSA keys with the provided bit size,
+// sets them as the PrivateKey and PublicKey of the RSAHandler, and returns them
+// in PEM format. A typical safe bit size is 2048.
+func (r *RSAHandler) NewPEMKeyPair(bitSize int) (privateKey, publicKey []byte, err error) {
+	privKey, err := rsa.GenerateKey(rand.Reader, bitSize)
+	if err != nil {
+		return nil, nil, err
+	}
+	r.PrivateKey = privKey
+
+	pubKey := privKey.PublicKey
+	r.PublicKey = &pubKey
+
+	privateKeyBytes := x509.MarshalPKCS1PrivateKey(privKey)
+	privateKeyBlock := &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: privateKeyBytes,
+	}
+	privateKeyPEM := pem.EncodeToMemory(privateKeyBlock)
+
+	publicKeyBytes, err := x509.MarshalPKIXPublicKey(&pubKey)
+	if err != nil {
+		return nil, nil, err
+	}
+	publicKeyBlock := &pem.Block{
+		Type:  "PUBLIC KEY",
+		Bytes: publicKeyBytes,
+	}
+	publicKeyPEM := pem.EncodeToMemory(publicKeyBlock)
+
+	return privateKeyPEM, publicKeyPEM, nil
+}
+
+// PublicKeyFromPEM takes a PEM-formatted RSA public key as input, decodes it,
+// sets it as the RSAHandler's public key, and returns it. If the provided key
+// is not a valid RSA public key, it returns an error.
+func (r *RSAHandler) PublicKeyFromPEM(pubkeyPEM []byte) (*rsa.PublicKey, error) {
+	block, _ := pem.Decode(pubkeyPEM)
+	if block == nil {
+		return nil, errors.New("failed to pem.Decode public key")
+	}
+
+	pub, err := x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+
+	publicKey, ok := pub.(*rsa.PublicKey)
+	if !ok {
+		return nil, errors.New("not an RSA public key")
+	}
+
+	r.PublicKey = publicKey
+	return publicKey, nil
+}
+
+// PrivateKeyFromPEM takes a PEM-formatted RSA private key as input, decodes it,
+// sets it as the RSAHandler's private key, and returns it. If the provided key
+// is not a valid RSA private key, it returns an error.
+func (r *RSAHandler) PrivateKeyFromPEM(privkeyPEM []byte) (*rsa.PrivateKey, error) {
+	block, _ := pem.Decode(privkeyPEM)
+	priv, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+
+	r.PrivateKey = priv
+	return priv, nil
+}
---
+++ rsa_test.go
@@ -0,0 +1,117 @@
+package structconf_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/kevinfalting/structconf"
+)
+
+func TestRSAHandlerMethods(t *testing.T) {
+	handler := &structconf.RSAHandler{}
+
+	// Generate a new key pair
+	privKeyPEM, pubKeyPEM, err := handler.NewPEMKeyPair(2048)
+	if err != nil {
+		t.Fatalf("failed to generate a new key pair: %v", err)
+	}
+
+	// Convert the private key back from PEM
+	_, err = handler.PrivateKeyFromPEM(privKeyPEM)
+	if err != nil {
+		t.Fatalf("failed to convert the private key from PEM: %v", err)
+	}
+
+	// Convert the public key back from PEM
+	_, err = handler.PublicKeyFromPEM(pubKeyPEM)
+	if err != nil {
+		t.Fatalf("failed to convert the public key from PEM: %v", err)
+	}
+
+	// Check if the keys are set in the handler
+	if handler.PrivateKey == nil {
+		t.Fatal("PrivateKey in the handler is not set")
+	}
+
+	if handler.PublicKey == nil {
+		t.Fatal("PublicKey in the handler is not set")
+	}
+
+	// Create a test plaintext
+	plaintext := "This is a test"
+
+	// Encrypt the plaintext
+	ciphertext, err := handler.Encrypt([]byte(plaintext))
+	if err != nil {
+		t.Fatalf("failed to encrypt the plaintext: %v", err)
+	}
+
+	// Decrypt the ciphertext
+	decrypted, err := handler.Decrypt(ciphertext)
+	if err != nil {
+		t.Fatalf("failed to decrypt the ciphertext: %v", err)
+	}
+
+	// Check if the decrypted text matches the original plaintext
+	if string(decrypted) != plaintext {
+		t.Fatalf("Decryption failed. Expected: %s, Got: %s", plaintext, string(decrypted))
+	}
+
+	type A struct {
+		Password string `conf:"secret"`
+	}
+
+	a := A{
+		Password: string(ciphertext),
+	}
+	fields, err := structconf.SettableFields(&a)
+	if err != nil {
+		t.Fatalf("failed to get SettableFields: %v", err)
+	}
+
+	if len(fields) != 1 {
+		t.Fatalf("expected 1 field, got %d", len(fields))
+	}
+
+	// Handle the field
+	decryptedValue, err := handler.Handle(context.Background(), fields[0], nil)
+	if err != nil {
+		t.Fatalf("failed to handle the field: %v", err)
+	}
+
+	// Check if the handled value matches the original plaintext
+	if decryptedValue != plaintext {
+		t.Fatalf("Handle method failed. Expected: %s, Got: %s", plaintext, decryptedValue)
+	}
+
+	plaintext = "Hello World!"
+
+	ciphertext, err = handler.Encrypt([]byte(plaintext))
+	if err != nil {
+		t.Fatalf("failed to encrypt the plaintext: %v", err)
+	}
+
+	decryptedValue, err = handler.Handle(context.Background(), fields[0], string(ciphertext))
+	if err != nil {
+		t.Fatalf("failed to handle the field: %v", err)
+	}
+
+	if decryptedValue != plaintext {
+		t.Fatalf("Handle method failed. Expected: %s, Got: %s", plaintext, decryptedValue)
+	}
+
+	var a1 A
+	fields, err = structconf.SettableFields(&a1)
+	if err != nil {
+		t.Fatalf("failed to get SettableFields: %v", err)
+	}
+
+	if len(fields) != 1 {
+		t.Fatalf("expected 1 field, got %d", len(fields))
+	}
+
+	_, err = handler.Handle(context.Background(), fields[0], nil)
+	if err != nil {
+		t.Fatalf("expected no error, got: %v", err)
+	}
+}
